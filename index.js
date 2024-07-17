1563c6be1b6b4976212d965c69d9ba6688319fb4
Anjaneya Kumar <131331479+Anjaneyakumar18@users.noreply.github.com>
2024-07-17 14:16:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,25 +22,25 @@ document.addEventListener("keypress",function(e){
 function makesound(key){
     switch(key){
         case "w":
-            new Audio("sounds/tom-1.mp3").play();
+            new Audio("./tom-1.mp3").play();
             break
         case "a":
-            new Audio("sounds/tom-2.mp3").play();
+            new Audio("./tom-2.mp3").play();
             break
         case "s":
-            new Audio("sounds/tom-3.mp3").play();
+            new Audio("./tom-3.mp3").play();
             break
         case "d":
             new Audio("sounds/tom-4.mp3").play();
             break
         case "j":
-            new Audio('sounds/crash.mp3').play();
+            new Audio('./crash.mp3').play();
             break
         case "k":
             new Audio('sounds/kick-bass.mp3').play();
             break
         case "l":
-            new Audio('sounds/snare.mp3').play();
+            new Audio('./snare.mp3').play();
             break
         default:
             console.log("invalid key");
